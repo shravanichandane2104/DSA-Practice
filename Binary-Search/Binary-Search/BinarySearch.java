8de9dc5caf798a6fa22a084406357a79d3b572a3
shravani chandane <shravichandane@gmail.com>
2026-08-10 19:13:37 +0530

    Add Binary Search solution

---
+++ Binary-Search/Binary-Search/BinarySearch.java
@@ -0,0 +1,34 @@
+/*
+Problem: 704. Binary Search
+Platform: LeetCode
+Difficulty: Easy
+Pattern: Binary Search
+
+Time Complexity: O(log n)
+Space Complexity: O(1)
+*/
+
+class BinarySearch {
+    public int search(int[] nums, int target) {
+
+        int low = 0;
+        int high = nums.length - 1;
+
+        while (low <= high) {
+
+            int mid = low + (high - low) / 2;
+
+            if (nums[mid] == target) {
+                return mid;
+            } 
+            else if (nums[mid] < target) {
+                low = mid + 1;
+            } 
+            else {
+                high = mid - 1;
+            }
+        }
+
+        return -1;
+    }
+}
