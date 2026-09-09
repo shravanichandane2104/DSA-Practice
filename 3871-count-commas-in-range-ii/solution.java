42c6b4378c767d852cd13321a53007ed7b6c90e6
shravani chandane <shravichandane@gmail.com>
2026-09-09 09:21:00 +0530

    Count Commas in Range II - 2026-09-09T03:50:59.275Z

---
+++ 3871-count-commas-in-range-ii/solution.java
@@ -0,0 +1,20 @@
+// 1 ms | 42.1 MB
+class Solution {
+    public long countCommas(long n) {
+
+        long ans = 0;
+        long x = 1000;
+
+        while (x <= n) {
+
+            ans += n - x + 1;
+
+            if (x > n / 1000)
+                break;
+
+            x *= 1000;
+        }
+
+        return ans;
+    }
+}
