b166584ca5eabe2b1f719d799e656fa46bd1d6f7
shravani chandane <shravichandane@gmail.com>
2026-09-13 11:43:22 +0530

    Image Overlap - 2026-09-13T06:13:21.420Z

---
+++ 835-image-overlap/solution.java
@@ -0,0 +1,35 @@
+// 60 ms | 44.6 MB
+class Solution {
+    public int largestOverlap(int[][] img1, int[][] img2) {
+
+        int n = img1.length;
+        int ans = 0;
+
+        for (int dr = -n + 1; dr < n; dr++) {
+            for (int dc = -n + 1; dc < n; dc++) {
+
+                int count = 0;
+
+                for (int i = 0; i < n; i++) {
+                    for (int j = 0; j < n; j++) {
+
+                        int ni = i + dr;
+                        int nj = j + dc;
+
+                        if (ni >= 0 && ni < n &&
+                            nj >= 0 && nj < n &&
+                            img1[i][j] == 1 &&
+                            img2[ni][nj] == 1) {
+
+                            count++;
+                        }
+                    }
+                }
+
+                ans = Math.max(ans, count);
+            }
+        }
+
+        return ans;
+    }
+}
